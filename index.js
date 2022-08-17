6fff3b09c10eb6d493aa84cc3b2f450580b03c35
zerocho <zerohch0+github@gmail.com>
2022-08-17 17:47:56 +0900

    Update: gcp-upload code

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const storage = new Storage();
 exports.resizeAndUpload = (data, context) => {
   const { bucket, name } = data;
 
-  if (!name.startsWith('original')) return;
+  if (!name.startsWith('original')) return 1;
 
   const ext = name.split('.').at(-1).toLowerCase();
   const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg사용합니다
-  console.log('name', name, 'ext', ext);
+  console.log('bucket', bucket, 'name', name, 'ext', ext);
 
   const file = storage.bucket(bucket).file(name);
   const readStream = file.createReadStream();
