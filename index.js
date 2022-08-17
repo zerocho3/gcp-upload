ae0d392eb1e4e0cc08f787b70a8e90158684bea6
zerocho <zerohch0+github@gmail.com>
2022-08-17 19:15:38 +0900

    Update: gcp-upload code

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const { Storage } = require('@google-cloud/storage');
 const sharp = require('sharp');
 
-const storage = new Storage();
 exports.resizeAndUpload = (data, context) => {
+  const storage = new Storage();
   const { bucket, name } = data;
 
   if (!name.startsWith('original')) return 1;
 
-  const ext = name.split('.').at(-1).toLowerCase();
+  const filename = name.split('/').at(-1);
+  const ext = filename.split('.').at(-1).toLowerCase();
   const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg사용합니다
-  console.log('bucket', bucket, 'name', name, 'ext', ext);
+  console.log('name', name, 'ext', ext);
 
-  const file = storage.bucket(bucket).file(name);
+  const file = storage.bucket(bucket).file(filename);
   const readStream = file.createReadStream();
 
-  const newFile = storage.bucket(bucket).file(name.replace(/^original/, 'thumb'));
+  const newFile = storage.bucket(bucket).file(`thumb/${filename}`);
   const writeStream = newFile.createWriteStream();
 
-  sharp(readStream)
-    .resize(200, 200, { fit: 'inside' })
-    .toFormat(requiredFormat)
+  readStream
+    .pipe(
+      sharp()
+        .resize(200, 200, { fit: 'inside' })
+        .toFormat(requiredFormat)
+    )
     .pipe(writeStream);
   return new Promise((resolve, reject) => {
     writeStream.on('finish', () => {
-      resolve(`thumb/${name}`);
+      resolve(`thumb/${filename}`);
     });
     writeStream.on('error', reject);
   });
